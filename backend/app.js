248e70f04cf7a80e368fe8b001e929fc05f40165
MUSTAFA KHAN <mustafakhan9743@gmail.com>
2024-08-07 16:17:45 +0530

    prepared for deployment

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,11 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDatabase } from "./config/dbconnect.js";
 import errorMiddleware from "./middlewares/errors.js";
+import path from "path";
+//import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //Handle uncaught exception
 process.on("uncaughtException", (err) => {
@@ -12,7 +17,9 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
-dotenv.config({ path: "backend/config/config.env" });
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  dotenv.config({ path: "backend/config/config.env" });
+}
 
 //connecting to database
 connectDatabase();
@@ -32,11 +39,20 @@ import productRoutes from "./routes/products.js";
 import authRoutes from "./routes/auth.js";
 import orderRoutes from "./routes/order.js";
 import paymentRoutes from "./routes/payment.js";
+import { fileURLToPath } from "url";
 
 app.use("/api/v1", productRoutes);
 app.use("/api/v1", authRoutes);
 app.use("/api/v1", orderRoutes);
 app.use("/api/v1", paymentRoutes);
+
+if (process.env.NODE_ENV === "PRODUCTION") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  });
+}
 
 //Using error Middleware
 app.use(errorMiddleware);
